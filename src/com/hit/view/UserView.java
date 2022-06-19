1e3d8e3313d3184eb3ea69243e4d6c3a812fefad
Sasha <sashamiliatin@gmail.com>
2022-06-19 17:02:46 +0300

    add buttons for user and admin

--- src/com/hit/view/UserView.java
+++ src/com/hit/view/UserView.java
@@ -1,21 +1,44 @@
 package com.hit.view;
-
+//import javafx.application.Application;
+//import javafx.scene.Scene;
+//import javafx.scene.web.WebView;
+//import javafx.stage.Stage;
+import chrriis.dj.nativeswing.swtimpl.NativeInterface;
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
 import com.hit.client.Song;
+import com.hit.driver.ButtonColumn;
 
+import javax.media.CannotRealizeException;
+import javax.media.NoPlayerException;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Observer;
+
+
 
 public class UserView extends JPanel implements ActionListener {
     private JTable songsTable;
     private GraphicalView gui;
-    private String[] songTableColumn = {"Name", "Artist", "Genre", "Link", ""};
+    private String[] songTableColumn = {"Name", "Artist", "Genre", "Link", "Play"};
     private JButton backButton, addSong;
     private JLabel jLabel;
+
+    public static JPanel getBrowserPanel(String url) {
+        JPanel webBrowserPanel = new JPanel(new BorderLayout());
+        JWebBrowser webBrowser = new JWebBrowser();
+        webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
+        webBrowser.setBarsVisible(false);
+        webBrowser.navigate(url);
+        return webBrowserPanel;
+    }
 
 
     public UserView(GraphicalView gui) {
@@ -45,17 +68,41 @@ public class UserView extends JPanel implements ActionListener {
         defaultTableModel.setColumnIdentifiers(songTableColumn);
 
         for (Song song : songs) {
-            String[] row = new String[4];
+            String[] row = new String[5];
             row[0] = song.getSongName();
             row[1] = song.getSongArtist();
             row[2] = song.getSongGenre();
             row[3] = song.getSongLink();
+            row[4] ="";
 
             defaultTableModel.addRow(row);
 
         }
+        Action play = new AbstractAction("Play") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTable table = (JTable) e.getSource();
+                int row = Integer.valueOf(e.getActionCommand());
+                try {
+                    Desktop.getDesktop().browse(new URI("https://"+table.getModel().getValueAt(row,3)));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (URISyntaxException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+        ButtonColumn buttonColumn = new ButtonColumn(songsTable, play, 4,"Play");
+        buttonColumn.setMnemonic(KeyEvent.VK_D);
     }
-
+    public static JPanel youtubePanel(String url){
+        JPanel wbPanel = new JPanel(new BorderLayout());
+        JWebBrowser wb = new JWebBrowser();
+        wbPanel.add(wb,BorderLayout.CENTER);
+        wb.setBarsVisible(false);
+        wb.navigate(url);
+        return wbPanel;
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         if ("back".equals(e.getActionCommand())) {
@@ -66,4 +113,13 @@ public class UserView extends JPanel implements ActionListener {
         }
 
     }
+//    MediaPlayer mediaPanel = new MediaPlayer( mediaUrl );
+//
+//mediaTest.add( mediaPanel );
+//
+//mediaTest.setSize( 800, 700 ); // set the size of the player
+//
+//mediaTest.setLocationRelativeTo(null);
+//
+//mediaTest.setVisible( true );
 }
